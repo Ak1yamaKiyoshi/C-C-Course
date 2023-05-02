dae0a921e0a40a26e0c3d73639505ca55f2f2259
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-05-03 00:03:43 +0300

    add bitfields for improved search

--- 2023.04.06 Trees/main.c
+++ 2023.04.06 Trees/main.c
@@ -3,19 +3,32 @@
 #include <stdio.h>
 #include <strings.h>
 
+
+const char NOTHING_TO_SEARCH = 0x0; /* 0b00000000 */
+const char USE_VALUE = 0x1;         /* 0b00000001 (1 - use, 0 - do not use) */
+const char DEPPEST = 0x2;           /* 0b00000010 (1 - lowerest/deepest, 0 - highest */
+const char USE_DEPTH = 0x4;         /* 0b00000100 (1 - use, 0 - do not use) */
+const char LEFT_RIGHT = 0x8;        /* 0b00001000 (1 - right child, 0 - left child */
+const char USE_CHILD_NODE = 0x10;   /* 0b00010000 (1 - use node pointer as child, 0 - do not use */
+const char USE_PARENT = 0x20;       /* 0b00100000 (1 - use node pointer as parent, 0 - do not use) */
+const char WITHOUT_CHILD = 0x40;    /* 0b01000000 (1- search for node without child, 0 - do not use) */
+const char WITH_CHILD = 0x80;       /* 0b10000000 (1 - search for node with child, 0 - do not use) */
+
 struct node {
   struct node *left;
   struct node *right;
   int value;
 };
 
+typedef struct node nnode;
+
 /*
  * allocate node with given value, left and right pointers will set on NULL
  * @param value int value to create node with
  * @return node allocated strcut node node with given value
  */
-struct node *allocateNode(int value) {
-  struct node *node = (struct node *)malloc(sizeof(struct node));
+nnode *allocateNode(int value) {
+  nnode *node = (nnode *)malloc(sizeof(nnode));
   node->value = value;
   node->left = NULL;
   node->right = NULL;
@@ -28,22 +41,22 @@ struct node *allocateNode(int value) {
  * @param counter distance to nearest node, ! set 0 if starting at root
  * @return left:right distance to nearest NULL child of this node
  */
-int nodeNearestNULLChild(struct node *tree, int counter) {
+int nearestNULL(nnode *tree, int counter) {
   if (tree == NULL)
     return counter;
-  int left = nodeNearestNULLChild(tree->left, counter + 1);
-  int right = nodeNearestNULLChild(tree->right, counter + 1);
+  int left = nearestNULL(tree->left, counter + 1);
+  int right = nearestNULL(tree->right, counter + 1);
   return (left <= right) ? left : right;
 }
 
 /*
  * rightmost insert node into the tree with given value
- * @param tree struct node* tree to insert value in
+ * @param tree nnode* tree to insert value in
  * @param value value to insert node in tree with
  * @return tree - ! assign return value only if youre creating tree, otherwise
  * it probably be subtree
  */
-struct node *insert(struct node *tree, int value) {
+nnode *insert(nnode *tree, int value) {
   if (tree == NULL)
     return allocateNode(value);
   /* check if left tree is not null, if so, insert node at left */
@@ -56,7 +69,7 @@ struct node *insert(struct node *tree, int value) {
     tree->right = allocateNode(value);
     return tree;
   }
-  (nodeNearestNULLChild(tree->left, 0) < nodeNearestNULLChild(tree->right, 0))
+  (nearestNULL(tree->left, 0) < nearestNULL(tree->right, 0))
       ? insert(tree->left, value)
       : insert(tree->right, value);
   return tree;
@@ -64,13 +77,13 @@ struct node *insert(struct node *tree, int value) {
 
 /*
  * rightmost insert array of values into the tree
- * @param tree struct node* tree to insert value in
+ * @param tree nnode* tree to insert value in
  * @param array array to insert in tree
  * @param len lenght of array that will be inserted in tree
  * @return tree - ! assign return value only if youre creating tree, otherwise
  * it probably be subtree
  */
-struct node *insertFromArray(struct node *tree, int *array, int len) {
+nnode *nInsertArray(nnode *tree, int *array, int len) {
   for (int i = 0; i < len; i++)
     if (tree == NULL)
       tree = insert(tree, array[i]);
@@ -80,43 +93,58 @@ struct node *insertFromArray(struct node *tree, int *array, int len) {
 }
 
 /*
- * serach node in tree using post order traverse
+ * search node in tree BY VALUE using post order traverse
  * @param tree tree to search value in
  * @param value value to search in list
  * @return node with given value or NULL
  */
-struct node *searchPostOrder(struct node *tree, int value) {
-
+nnode *nFindByValue(nnode *tree, int value) {
   if (tree != NULL) {
-    struct node *buffer = searchPostOrder(tree->left, value);
+    /* seach node in left subtree */
+    nnode *buffer = nFindByValue(tree->left, value);
 
+    /* if node found in previous call, return it */
     if (buffer != NULL && buffer->value == value)
       return buffer;
 
-    buffer = searchPostOrder(tree->right, value);
+    /* seach node in right subtree */
+    buffer = nFindByValue(tree->right, value);
 
+    /* if node found in previous call, return it */
     if (buffer != NULL && buffer->value == value)
       return buffer;
 
-    if (tree->value == value)
-      return tree;
-
-    else
-      return NULL;
+    /* found node with right value */ 
+    if (tree->value == value) return tree;
+  
+    /* node not found */
+    else return NULL;
   }
   return NULL;
 }
+
+/* 
+const char NOTHING_TO_SEARCH = 0x0; // 0b00000000 
+const char USE_VALUE = 0x1;         // 0b00000001 (1 - use, 0 - do not use) 
+const char DEPPEST = 0x2;           // 0b00000010 (1 - lowerest/deepest, 0 - highest 
+const char USE_DEPTH = 0x4;         // 0b00000100 (1 - use, 0 - do not use) 
+const char LEFT_RIGHT = 0x8;        // 0b00001000 (1 - right child, 0 - left child 
+const char USE_CHILD_NODE = 0x10;   // 0b00010000 (1 - use node pointer as child, 0 - do not use 
+const char USE_PARENT = 0x20;       // 0b00100000 (1 - use node pointer as parent, 0 - do not use) 
+const char WITHOUT_CHILD = 0x40;    // 0b01000000 (1- search for node without child, 0 - do not use) 
+const char WITH_CHILD = 0x80;       // 0b10000000 (1 - search for node with child, 0 - do not use) 
+*/
 
 /*
  * find node with minimal value in tree
  * @param tree to search in
  * @return minNode node with min value
  */
-struct node *min(struct node *tree) {
+nnode *min(nnode *tree) {
   if (tree != NULL) {
 
-    struct node *minNode = tree;
-    struct node *buffer = min(tree->left);
+    nnode *minNode = tree;
+    nnode *buffer = min(tree->left);
     if (buffer != NULL && minNode->value > buffer->value)
       minNode = buffer;
     buffer = min(tree->right);
@@ -134,15 +162,15 @@ struct node *min(struct node *tree) {
  * @param nodeToFind parent of that node will be found if exists
  * @return tree parent of nodeToFInd
  */
-struct node *findParentOfNode(struct node *tree, struct node *nodeToFind) {
+nnode *nodeParent(nnode *tree, nnode *nodeToFind) {
   if (tree != NULL) {
     if (tree->left == nodeToFind || tree->right == nodeToFind)
       return tree;
-    struct node *buffer = findParentOfNode(tree->left, nodeToFind);
+    nnode *buffer = nodeParent(tree->left, nodeToFind);
     if (buffer != NULL &&
         (buffer->left == nodeToFind || buffer->right == nodeToFind))
       return buffer;
-    buffer = findParentOfNode(tree->right, nodeToFind);
+    buffer = nodeParent(tree->right, nodeToFind);
     if (buffer != NULL &&
         (buffer->left == nodeToFind || buffer->right == nodeToFind))
       return buffer;
@@ -156,15 +184,14 @@ struct node *findParentOfNode(struct node *tree, struct node *nodeToFind) {
  * @param tree tree to remove node from
  * @param value value of node to remove
  * @return tree - pointer to tree  
- * TODO: delete from root
 */
-struct node *removeLast(struct node *tree, int value) {
+nnode *nRemoveByValue(nnode *tree, int value) {
   // 1. find node with given value 
-  struct node *nodePtr = searchPostOrder(tree, value);
+  nnode *nodePtr = nFindByValue(tree, value);
 
   // 2. find minimum value of it's subtrees 
-  struct node *minNode = min(nodePtr->left);
-  struct node *bufferNode = min(nodePtr->right);
+  nnode *minNode = min(nodePtr->left);
+  nnode *bufferNode = min(nodePtr->right);
 
   // 3. assign minimum value to minNode 
   if ( minNode != NULL && bufferNode != NULL) 
@@ -176,7 +203,7 @@ struct node *removeLast(struct node *tree, int value) {
   
   // 4.find parent of minNode
   if (minNode != NULL)
-    bufferNode = findParentOfNode(tree, minNode);
+    bufferNode = nodeParent(tree, minNode);
 
   // 5. assign value of minimum node to current node 
   if ( nodePtr != minNode )
@@ -190,7 +217,7 @@ struct node *removeLast(struct node *tree, int value) {
   else if (bufferNode != NULL) {
     free(bufferNode->right);
     bufferNode->right = NULL;
-  }
+  } 
 
   return tree;
 }
@@ -199,36 +226,36 @@ struct node *removeLast(struct node *tree, int value) {
  * print binary tree in array look using pre     order traverse
  * @param tree tree to print
  */
-void printPreOrder(struct node *tree) {
+void nPrintPreOrder(nnode *tree) {
   if (tree == NULL)
     return;
-  printPreOrder(tree->left);
+  nPrintPreOrder(tree->left);
   printf("%d ", tree->value);
-  printPreOrder(tree->right);
+  nPrintPreOrder(tree->right);
 }
 
 /*
  * print binary tree in array look using in order traverse
  * @param tree tree to print
  */
-void printInOrder(struct node *tree) {
+void nPrintInOrder(nnode *tree) {
   if (tree == NULL)
     return;
   printf("%d ", tree->value);
-  printInOrder(tree->left);
-  printInOrder(tree->right);
+  nPrintInOrder(tree->left);
+  nPrintInOrder(tree->right);
 }
 
 /*
  * print binary tree in array look using post order traverse
  * @param tree tree to print
  */
-void printPostOrder(struct node *tree) {
+void nPrintPostOrder(nnode *tree) {
   if (tree == NULL)
     return;
   printf("%d ", tree->value);
-  printPostOrder(tree->right);
-  printPostOrder(tree->left);
+  nPrintPostOrder(tree->right);
+  nPrintPostOrder(tree->left);
 }
 
 /*
@@ -237,17 +264,17 @@ void printPostOrder(struct node *tree) {
  * @param space spacing between nodes in print (heigher spacing, taller the tree
  * will be)
  */
-void fancyPrint(struct node *tree, int space) {
+void nFancyPrint(nnode *tree, int space) {
   if (tree == NULL)
     return;
   space += 5;
-  fancyPrint(tree->right, space);
+  nFancyPrint(tree->right, space);
   printf("\n");
   for (int i = 5; i < space; i++) {
     printf(" ");
   }
   printf("[%d]<\n", tree->value);
-  fancyPrint(tree->left, space);
+  nFancyPrint(tree->left, space);
 }
 
 /*
@@ -255,66 +282,69 @@ void fancyPrint(struct node *tree, int space) {
  * @param tree tree to calculate path in 
  * @return max sum of node values path to leaf node in tree 
 */
-int calculateMaxTreePath(struct node* tree) {
+int nCalculateMaxTreePath(nnode* tree) {
   if (tree == NULL) return 0;
-  int left = calculateMaxTreePath(tree->left);
-  int right = calculateMaxTreePath(tree->right);
+  int left = nCalculateMaxTreePath(tree->left);
+  int right = nCalculateMaxTreePath(tree->right);
   return tree->value + ((left > right) ? left : right);
 }
 
+
+
+
 void testInsert() {
-struct node *tree = NULL;
+nnode *tree = NULL;
   int array[] = {0,  1,  2,  3,  4,  5,   6,     7,    8,   -100,
                  9,  10, 11, 12, 13, -90, 14,    15,   16,  17,
                  18, 19, 20, 21, 22, 23,  -2000, -900, -400};
-  tree = insertFromArray(tree, array, 28);
+  tree = nInsertArray(tree, array, 28);
 
   printf("Tree print: ");
-  fancyPrint(tree, 1);
+  nFancyPrint(tree, 1);
   printf("\nTree traverses print: ");
   printf("\ntree pre order traversal:  ");
-  printPreOrder(tree);
+  nPrintPreOrder(tree);
   printf("\ntree in order traversal:   ");
-  printInOrder(tree);
+  nPrintInOrder(tree);
   printf("\ntree post order traversal: ");
-  printPostOrder(tree);
+  nPrintPostOrder(tree);
   printf("\n node with value 3 [post order search]: %d",
-  searchPostOrder(tree, 3)->value);
+  nFindByValue(tree, 3)->value);
   printf("\nminimum of the tree: %d\n", min(tree)->value);
   printf("\n parent of min value: %d",
-  findParentOfNode(tree, min(tree))->value);
+  nodeParent(tree, min(tree))->value);
 }
 
 void testRemove() {
-struct node *tree = NULL;
+nnode *tree = NULL;
   int array[] = {1, 2, 3, 4, 5, 6};
-  tree = insertFromArray(tree, array, 6); 
+  tree = nInsertArray(tree, array, 6); 
 
   // two child case
-  removeLast(tree, 3); 
+nRemoveByValueremoveLast(tree, 3); 
   // one child case
-  removeLast(tree, 4);
+nRemoveByValueremoveLast(tree, 4);
   // terminal node case 
-  removeLast(tree, 5);
+nRemoveByValueremoveLast(tree, 5);
   // root case 
-  // removeLast(tree, 1);
+nRemoveByValueremoveLast(tree, 1);
 
   printf("Tree print: ");
-  fancyPrint(tree, 1);
+  nFancyPrint(tree, 1);
 }
 
 void testMaxPath() {
-  struct node *tree = NULL;
+  nnode *tree = NULL;
   int array[] = {0,  1,  100,  3,  4, 5, 70};
-  tree = insertFromArray(tree, array, 7);
-  printf("max path: %d \n ", calculateMaxTreePath(tree));
+  tree = nInsertArray(tree, array, 7);
+  printf("max path: %d \n ", nCalculateMaxTreePath(tree));
   
   printf("Tree print: ");
-  fancyPrint(tree, 1);
+  nFancyPrint(tree, 1);
 }
 
 int main() {
-
+  //testInsert();
   testRemove();
 
   return 0;
